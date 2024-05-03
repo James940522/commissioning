5e3850e40281e2414b2c25986e4eec1b82a65ad9
Jaemin Jung <james940522@gmail.com>
2024-05-03 10:53:32 +0900

    Fix: 서버 내용 수정 했지만...

--- protopie-layout/index.js
+++ protopie-layout/index.js
@@ -1,36 +1,49 @@
 const express = require('express');
+const cors = require('cors');
 const puppeteer = require('puppeteer');
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Route to render mobile page
-app.get('/mobile-page', async (req, res) => {
+// CORS 설정
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
+
+// 페이지 렌더링 처리
+app.post('/render', async (req, res) => {
+  const { url, userAgent, size } = req.body;
+
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    // Set user agent for mobile device
-    await page.setUserAgent(
-      'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
-    );
-
-    // Set viewport for mobile dimensions
-    await page.setViewport({ width: 375, height: 667 });
-
-    await page.goto(`https://cloud.protopie.io/p/aac5c7bd3f`, {
-      waitUntil: 'networkidle2',
+    // 뷰포트 설정
+    await page.setViewport({
+      width: size.width,
+      height: size.height,
+      deviceScaleFactor: 1,
     });
-    const content = await page.content(); // Get page content as HTML
 
+    await page.setUserAgent(userAgent);
+    await page.goto(url, { waitUntil: 'networkidle0' });
+
+    // 페이지 내에서 CSS를 조정하여 원하는 크기로 강제 설정
+    await page.evaluate(size => {
+      document.body.style.width = `${size.width}px`;
+      document.body.style.height = `${size.height}px`;
+    }, size);
+
+    const content = await page.content();
     await browser.close();
-    res.send(content);
+    res.send({ html: content });
   } catch (error) {
     console.error(error);
-    res.status(500).send('Failed to retrieve page');
+    res.status(500).send('Failed to render the page');
   }
 });
 
